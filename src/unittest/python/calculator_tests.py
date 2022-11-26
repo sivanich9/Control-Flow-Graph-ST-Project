0fb53f1dec6e54f97af0ae62b5590fd3881c56e3
sivanich9 <56951616+sivanich9@users.noreply.github.com>
2022-11-26 15:37:16 +0530

    Update calculator_tests.py

--- src/unittest/python/calculator_tests.py
+++ src/unittest/python/calculator_tests.py
@@ -7,23 +7,49 @@ import unittest
 from matrixCalculator import calculator
 
 class MatrixCalculatorTest(unittest.TestCase):
+   
+    #scalar operations test cases
     def test_scalar_addition(self):
         actual_output = calculator(1,1,[[1,2,3],[4,5,6],[7,8,9]],5)
         expected_output = [[6,7,8],[9,10,11],[12,13,14]]
         np.testing.assert_array_equal(actual_output, expected_output)
     
+    def test_scalar_subtraction(self):
+        actual_output = calculator(1,2,[[5,6,7],[8,9,10],[11,12,13]],3)
+        expected_output = [[2,3,4],[5,6,7],[8,9,10]]
+        np.testing.assert_array_equal(actual_output, expected_output)
+        
+    def test_scalar_multiplication(self):
+        actual_output = calculator(1,3,[[1,3,5],[7,9,11],[13,15,17]],2)
+        expected_output = [[2,6,10],[14,18,22],[26,30,34]]
+        np.testing.assert_array_equal(actual_output, expected_output)
+        
+    def test_scalar_division(self):
+        actual_output = calculator(1,4,[[2,4,6],[8,10,12],[14,16,18]],2)
+        expected_output = [[1,2,3],[4,5,6],[7,8,9]]
+        np.testing.assert_array_equal(actual_output, expected_output)
     
-    
+        
+    #vector operation test cases    
     def test_vector_addition(self):
         actual_output = calculator(2,1,[[1,2,3],[4,5,6],[7,8,9]],[[1,2,3],[4,5,6],[7,8,9]])
         expected_output = [[2,4,6],[8,10,12],[14,16,18]]
         np.testing.assert_array_equal(actual_output, expected_output)
 
+    def test_vector_subtraction(self):
+        actual_output = calculator(2,2,[[10,11,12],[13,14,15],[16,17,18]],[[1,2,3],[4,5,6],[7,8,9]])
+        expected_output = [[9,9,9],[9,9,9],[9,9,9]]
+        np.testing.assert_array_equal(actual_output, expected_output)
+    
 
-    def test_vector_multiplication(self):
+    def test_vector_multiplication1(self):
         actual_output = calculator(2,1,[[1,2,3],[4,5,6],[7,8,9]],[[1,2,3],[4,5,6],[7,8,9]])
         expected_output = [[30,36,42],[66,81,96],[102,126,150]]
         np.testing.assert_array_equal(actual_output, expected_output)
+        
+        
+        
+        
 
 
         
