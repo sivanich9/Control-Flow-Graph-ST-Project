286dc8cf088cbd29db894907c695af5ffc6b3088
sivanich9 <56951616+sivanich9@users.noreply.github.com>
2022-11-27 18:08:29 +0530

    Update calculator_tests.py

--- src/unittest/python/calculator_tests.py
+++ src/unittest/python/calculator_tests.py
@@ -8,7 +8,8 @@ from matrixCalculator import calculator
 
 class MatrixCalculatorTest(unittest.TestCase):
    
-    #scalar operations test cases
+    #Scalar operations test cases
+      
     def test_scalar_addition(self):
         actual_output = calculator(1,1,[[1,2,3],[4,5,6],[7,8,9]],5)
         expected_output = [[6,7,8],[9,10,11],[12,13,14]]
@@ -30,7 +31,8 @@ class MatrixCalculatorTest(unittest.TestCase):
         np.testing.assert_array_equal(actual_output, expected_output)
     
         
-    #vector operation test cases    
+    #Vector operation test cases    
+   
     def test_vector_addition(self):
         actual_output = calculator(2,1,[[1,2,3],[4,5,6],[7,8,9]],[[1,2,3],[4,5,6],[7,8,9]])
         expected_output = [[2,4,6],[8,10,12],[14,16,18]]
@@ -41,13 +43,50 @@ class MatrixCalculatorTest(unittest.TestCase):
         expected_output = [[9,9,9],[9,9,9],[9,9,9]]
         np.testing.assert_array_equal(actual_output, expected_output)
     
-
     def test_vector_multiplication1(self):
+        actual_output = calculator(2,1,[],[])
+        expected_output = []
+        np.testing.assert_array_equal(actual_output, expected_output)
+         
+    def test_vector_multiplication2(self):
+        actual_output = calculator(2,1,[2],[3])
+        expected_output = [6]
+        np.testing.assert_array_equal(actual_output, expected_output)    
+   
+    def test_vector_multiplication3(self):
         actual_output = calculator(2,1,[[1,2,3],[4,5,6],[7,8,9]],[[1,2,3],[4,5,6],[7,8,9]])
         expected_output = [[30,36,42],[66,81,96],[102,126,150]]
         np.testing.assert_array_equal(actual_output, expected_output)
+      
+    def test_vector_multiplication4(self):
+        actual_output = calculator(2,1,[[1,2,3],[4,5,6]],[[7,8],[9,10]])
+        expected_output = pass
+        np.testing.assert_array_equal(actual_output, expected_output)   
         
-        
+    #Singular matrix operation test cases
+   
+    def test_singular_transpose1(self):
+        actual_output = calculator(3,1,[])
+        expected_output = []
+        np.testing.assert_array_equal(actual_output, expected_output)
+      
+    def test_singular_transpose2(self):
+        actual_output = calculator(3,1,[5])
+        expected_output = [5]
+        np.testing.assert_array_equal(actual_output, expected_output)
+         
+    def test_singular_transpose3(self):
+        actual_output = calculator(3,1,[[1,2,3],[4,5,6]])
+        expected_output = [[1,4],[2,5],[3,6]]
+        np.testing.assert_array_equal(actual_output, expected_output)     
+      
+      
+    #Stream operation test cases
+   
+   
+    #Exit  
+   
+    
         
         
 
